b78a03b852df7ecfb4c7fb49567f19987817c97e
Julian Valentin <julian.valentin@bsplines.org>
2020-09-04 18:21:49 +0200

    Use nomarkdown for wrapper div

--- gh-pages/tools/generateWebsite.py
+++ gh-pages/tools/generateWebsite.py
@@ -131,15 +131,16 @@ def extractSections(lecture):
 
     sectionHtml = re.sub(r"<p>\s*(?:&#x2003;)+[\s\u0083]*\Z", "", sectionHtml)
 
-    # Due to some weird bug, I can't get Jekyll/Liquid to not escape the </div> tag
-    # if placed at the end (even inside the raw environment). So we'll just leave it open.
     sectionHtml = f"""
+{{::nomarkdown}}
 <div class="lwarp-contents">
 {{% raw %}}
 {mathJaxDefinitions}
 
 {sectionHtml}
 {{% endraw %}}
+</div>
+{{:/nomarkdown}}
 """
     writeFile(os.path.join("_includes", "lectures", lecture, f"{slug}.html"), sectionHtml)
 
