fc88ba8b6f3e606efe92ef6f3ae2b96c2caf8263
Julian Valentin <julian.valentin@bsplines.org>
2020-09-04 21:03:50 +0200

    Fix various errors in pushWebsite

--- gh-pages/tools/pushWebsite.py
+++ gh-pages/tools/pushWebsite.py
@@ -8,13 +8,16 @@ def main():
   if os.path.isdir(".git"): shutil.rmdir(".git")
 
   subprocess.run(["git", "init", "-b", "gh-pages"])
-  subprocess.run(["git", "checkout", "--orphan", "gh-pages"])
   subprocess.run(["git", "add", "."])
+  subprocess.run(["git", "add", "-f", "_data/sidebars/sidebar.yml"])
   subprocess.run(["git", "add", "-f", "_includes/lectures"])
   subprocess.run(["git", "add", "-f", "images/lectures"])
-  subprocess.run(["git", "add", "-f", "pages/lectures"])
-  subprocess.run(["git", "remote", "add", "git@github.com:valentjn/class-notes.git"])
+  subprocess.run(["git", "add", "-f", "pages/index.md", "pages/lectures"])
+  subprocess.run(["git", "commit", "-m", "Update gh-pages"])
+  subprocess.run(["git", "remote", "add", "origin", "git@github.com:valentjn/class-notes.git"])
   subprocess.run(["git", "push", "-f", "origin", "gh-pages"])
+
+  shutil.rmdir(".git")
 
 
 
