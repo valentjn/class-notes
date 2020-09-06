ad40349aeeee55ad59b7f3d1bb746b3fd9ca0eda
Julian Valentin <julian.valentin@bsplines.org>
2020-09-06 19:11:17 +0200

    Add TOC to pushWebsite

--- gh-pages/tools/pushWebsite.py
+++ gh-pages/tools/pushWebsite.py
@@ -13,7 +13,8 @@ def main():
   subprocess.run(["git", "add", "-f", "_data/sidebars/sidebar.yml"])
   subprocess.run(["git", "add", "-f", "_includes/lectures"])
   subprocess.run(["git", "add", "-f", "images/lectures"])
-  subprocess.run(["git", "add", "-f", "pages/index.md", "pages/lectures"])
+  subprocess.run(["git", "add", "-f", "pages/index.md", "pages/table-of-contents.md",
+      "pages/lectures"])
   subprocess.run(["git", "commit", "-m", "Update gh-pages"])
   subprocess.run(["git", "remote", "add", "origin", "git@github.com:valentjn/class-notes.git"])
   subprocess.run(["git", "push", "-f", "origin", "gh-pages"])
